d39e2dadd87e6eb515c2068ff1d6423017b80235
ankoi0310 <huynhvahuuan3620@gmail.com>
2022-01-11 22:25:39 +0700

    Sửa phần gửi mail

--- src/main/java/vn/edu/hcmuaf/fit/helper/MailSender.java
+++ src/main/java/vn/edu/hcmuaf/fit/helper/MailSender.java
@@ -7,10 +7,10 @@ import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
 public class MailSender {
-    private static String username = "cuongla253@gmail.com";
-    private static String password = "fqwmadurjvyhbder";
+    private static final String username = "huynhvahuuan3620@gmail.com";
+    private static final String password = "wetufudbauzfojyx";
 
-    public static boolean sendMail(String to, String subject, String content){
+    public static boolean sendMail(String to, String subject, String content) {
         Session session = connect();
         try {
             Message message = new MimeMessage(session);
@@ -25,22 +25,20 @@ public class MailSender {
             return false;
         }
     }
-    private static Session connect(){
+    private static Session connect() {
         Properties props = new Properties();
-        props.put("mail.smtp.auto","true");
+        props.put("mail.smtp.auth","true");
         props.put("mail.smtp.starttls.enable","true");
-        props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSlSocketFactory");
+        props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.host","smtp.gmail.com");
         props.put("mail.smtp.port","587");
-        return Session.getInstance(props, new javax.mail.Authenticator(){
-            protected PasswordAuthentication getPasswordAuthentication(){
-                return new PasswordAuthentication(username,password);
+        return Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
             }
         });
     }
     public static  void main (String[] args){
-        System.out.println(sendMail("cuongla253@gmail.com","Test Mail","\"Dear mail crawler\\n\\n No span to my email,please!\"")?"Send mail success":"Send mail fail");
+        System.out.println(sendMail("chilungdaica@gmail.com","Test Mail","\"Dear mail crawler\\n\\n No span to my email,please!\"")?"Send mail success":"Send mail fail");
     }
 }
-
-
